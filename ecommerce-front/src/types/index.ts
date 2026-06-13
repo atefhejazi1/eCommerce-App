c17d1867fae42295a1716479f8ed63c60d3a9aa0
Atef Hejazi <78237128+atefhejazi1@users.noreply.github.com>
2026-06-13 09:07:13 +0300

    fix: bypass tsc check on build

--- ecommerce-front/src/types/index.ts
+++ ecommerce-front/src/types/index.ts
@@ -3,4 +3,9 @@ import { type TLoading } from "./shared.types";
 import { type TCategory } from "./category.types";
 import { type TOrderItem } from "./order.types";
 import { isString } from "./guards";
-export { TProduct, isString, TLoading, TCategory, TOrderItem };
+
+// 1. تصدير الأنواع كأنواع بشكل منفصل (هنا الحل للخطأ)
+export type { TProduct, TLoading, TCategory, TOrderItem };
+
+// 2. تصدير الدالة كقيمة عادية
+export { isString };
